7f18560ccfccf07511d935e9a28bcd52296a53cb
Adler <1101635162@qq.com>
2019-09-12 15:20:26 +0800

    RMD DELE

--- src/pers/adlered/liteftpd/analyze/CommandAnalyze.java
+++ src/pers/adlered/liteftpd/analyze/CommandAnalyze.java
@@ -248,6 +248,44 @@ public class CommandAnalyze {
                         } else {
                             send.send("550 " + getLockPath(completePath, Permission.defaultDir) + ": Failed to create." + Dict.newLine);
                         }
+                    } else if (cmd.equals("RMD")) {
+                        String completePath = arg1;
+                        if (arg2 != null) {
+                            for (int i = 2; i < split.length; i++) {
+                                completePath += " " + split[i];
+                            }
+                        }
+                        if (completePath.indexOf("../") != -1) {
+                            completePath = completePath.replaceAll("\\.\\./", "");
+                        }
+                        //System.out.println("Complete path: " + completePath);
+                        completePath = getAbsolutePath(completePath);
+                        File file = new File(completePath);
+                        if (file.isDirectory()) {
+                            delFolder(completePath);
+                            send.send("250 RMD command successful." + Dict.newLine);
+                        } else {
+                            send.send(Dict.noSuchFileOrDir + getLockPath(completePath, Permission.defaultDir) + Dict.noSuchFIleOrDir2);
+                        }
+                    } else if (cmd.equals("DELE")) {
+                        String completePath = arg1;
+                        if (arg2 != null) {
+                            for (int i = 2; i < split.length; i++) {
+                                completePath += " " + split[i];
+                            }
+                        }
+                        if (completePath.indexOf("../") != -1) {
+                            completePath = completePath.replaceAll("\\.\\./", "");
+                        }
+                        //System.out.println("Complete path: " + completePath);
+                        completePath = getAbsolutePath(completePath);
+                        File file = new File(completePath);
+                        if (file.isFile()) {
+                            file.delete();
+                            send.send("250 DELE command successful." + Dict.newLine);
+                        } else {
+                            send.send(Dict.noSuchFileOrDir + getLockPath(completePath, Permission.defaultDir) + Dict.noSuchFIleOrDir2);
+                        }
                     }
                     /**
                      * TRANSMISSION COMMANDS
@@ -429,7 +467,7 @@ public class CommandAnalyze {
         } else {
             path = currentPath + "/" + path;
         }
-        //System.out.println("Absolute path: " + path);
+        System.out.println("Absolute path: " + path);
         return path;
     }
 
@@ -465,5 +503,46 @@ public class CommandAnalyze {
         map.put("calcPort", calcPort);
         map.put("finalPort", finalPort);
         return map;
+    }
+
+    public static void delFolder(String folderPath) {
+        try {
+            delAllFile(folderPath); //删除完里面所有内容
+            String filePath = folderPath;
+            filePath = filePath.toString();
+            java.io.File myFilePath = new java.io.File(filePath);
+            myFilePath.delete(); //删除空文件夹
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean delAllFile(String path) {
+        boolean flag = false;
+        File file = new File(path);
+        if (!file.exists()) {
+            return flag;
+        }
+        if (!file.isDirectory()) {
+            return flag;
+        }
+        String[] tempList = file.list();
+        File temp = null;
+        for (int i = 0; i < tempList.length; i++) {
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + tempList[i]);
+            } else {
+                temp = new File(path + File.separator + tempList[i]);
+            }
+            if (temp.isFile()) {
+                temp.delete();
+            }
+            if (temp.isDirectory()) {
+                delAllFile(path + "/" + tempList[i]);//先删除文件夹里面的文件
+                delFolder(path + "/" + tempList[i]);//再删除空文件夹
+                flag = true;
+            }
+        }
+        return flag;
     }
 }
